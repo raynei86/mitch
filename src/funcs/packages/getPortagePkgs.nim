c6690158524ddbb6a2c6f9ebdaf68f44ccf90d0f
Lihui Zhang <raynei@debian-BULLSEYE-live-builder-AMD64>
2022-10-29 11:15:01 -0400

    fix: fixed package count on Gentoo

--- src/funcs/packages/getPortagePkgs.nim
+++ src/funcs/packages/getPortagePkgs.nim
@@ -3,6 +3,6 @@ import
 
 proc getPortagePkgs*(): string =
   let
-    count = osproc.execCmdEx("ls -d /var/db/pkg/*/*| cut -f5- -d/")[0]
+    count = osproc.execCmdEx(r"ls -d /var/db/pkg/*/*| cut -f ""5"" -d ""/"" ")[0]
 
   result = $(count.split("\n").len - 1)
